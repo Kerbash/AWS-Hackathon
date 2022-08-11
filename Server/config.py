0491f3a8ed246dc9730dd62b70518632a5d0b8d1
William Selke <74438653+Kerbash@users.noreply.github.com>
2022-08-11 13:55:45 -0700

    Delete Server directory

--- Server/config.py
+++
@@ -1,8 +0,0 @@
-import pathlib
-
-class Config:
-    # IP of where its being hosted
-    HOSTNAME = "10.0.0.206"
-
-    # get the absolute path to the static files, js, css, etc
-    STATIC_FILE_PATH = str(pathlib.Path(__file__).parent.absolute()) + "\html\static"
